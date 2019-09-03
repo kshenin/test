85e78b1c7b1a6f287e77954c8fa7ec6855cc507c
Evgeniy <50575697+kshenin@users.noreply.github.com>
2019-09-03 19:27:16 +0300

    Revert "first commit"
    This reverts commit 909449aa8e2a91e5eeb7927c89e5642b4f18caff.

--- springkshenindemo/src/main/java/com/kshenin/demo/dao/EmployeeDAO.java
+++
@@ -1,8 +0,0 @@
-package com.kshenin.demo.dao;
-
-import com.kshenin.demo.model.EmployeeVO;
-import java.util.List;
-
-public interface EmployeeDAO {
-	List<EmployeeVO> getAllEmployees();
-}
--- springkshenindemo/src/main/java/com/kshenin/demo/service/EmployeeManager.java
+++
@@ -1,8 +0,0 @@
-package com.kshenin.demo.service;
-
-import com.kshenin.demo.model.EmployeeVO;
-import java.util.List;
-
-public interface EmployeeManager {
-	List<EmployeeVO> getAllEmployees();
-}
